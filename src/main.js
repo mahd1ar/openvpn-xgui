b162cdbd764f469f4e2d8656f553284340a55456
mahd1ar <a.mahdiyar7@yahoo.com>
2021-01-13 18:31:12 +0330

    command runner and some initials configuration are ok

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+// import { ipcRenderer } from 'electron'
+
+// window.ipcRenderer = ipcRenderer
 
 createApp(App)
   .use(store)
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,40 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <pre>
+      {{ ls }}
+    </pre>
   </div>
 </template>
+
+<script>
+import { ipcRenderer } from "electron";
+
+function exec(args) {
+  return new Promise((resolve, reject) => {
+    ipcRenderer.send("send_command", args); // prints "pong"
+
+    ipcRenderer.on("reply_command", (event, arg) => {
+      resolve(arg);
+    });
+
+    ipcRenderer.on("error_command", (event, arg) => {
+      reject(arg);
+    });
+  });
+}
+
+// window.ipcRenderer = ipcRenderer
+export default {
+  data() {
+    return {
+      ls: "ss"
+    };
+  },
+  async mounted() {
+    this.ls = await exec(["ls", "-lh", "/usr"]);
+  }
+};
+</script>
+
+<style></style>
